e170bdb97476c74c51c5aeb40e422b1a8bdea80e
tqcq <99722391+tqcq@users.noreply.github.com>
2024-03-24 11:26:28 +0800

    feat support make_unique for ptr

--- include/sled/make_unique.h
+++ include/sled/make_unique.h
@@ -2,6 +2,7 @@
 #define SLED_MAKE_UNIQUE_H
 #pragma once
 #include <memory>
+#include <type_traits>
 
 namespace sled {
 template<typename T, typename... Args>
@@ -9,6 +10,14 @@ inline auto
 MakeUnique(Args &&...args) -> std::unique_ptr<T>
 {
     return std::move(std::unique_ptr<T>(new T(std::forward<Args>(args)...)));
+}
+
+template<typename T, typename U = T>
+inline typename std::enable_if<!std::is_pointer<T>::value && !std::is_pointer<U>::value && std::is_base_of<T, U>::value,
+                               std::unique_ptr<T>>::type
+MakeUnique(U *ptr)
+{
+    return std::move(std::unique_ptr<T>(ptr));
 }
 
 }// namespace sled
